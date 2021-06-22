6220f85712253f9518d25b393076c4cca7489c83
Marlon Clay <luciousmc@icloud.com>
2021-06-22 16:00:31 -0700

    project initialized with vite added redux

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDRaUHTLkScHCGz4SFXBUBYxXg52gHJoZc",
+  authDomain: "myflix-5b646.firebaseapp.com",
+  projectId: "myflix-5b646",
+  storageBucket: "myflix-5b646.appspot.com",
+  messagingSenderId: "596754025210",
+  appId: "1:596754025210:web:d9b8f3502635d1c74ed743"
+};
+
