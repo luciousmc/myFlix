c36fa19008008ec6c58a2d9bd0395281062f0c55
Marlon Clay <luciousmc@icloud.com>
2021-06-23 13:33:02 -0700

    firebase config updated

--- firebase.js
+++ firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
   apiKey: "AIzaSyDRaUHTLkScHCGz4SFXBUBYxXg52gHJoZc",
   authDomain: "myflix-5b646.firebaseapp.com",
@@ -7,3 +9,8 @@ const firebaseConfig = {
   appId: "1:596754025210:web:d9b8f3502635d1c74ed743"
 };
 
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
