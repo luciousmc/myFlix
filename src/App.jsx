c7651e64b413d0d903925c936e91a0474be1d667
Marlon Clay <luciousmc@icloud.com>
2021-06-23 11:59:40 -0700

    updated folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import HomeScreen from './HomeScreen';
+import HomeScreen from './pages/HomeScreen';
 import {
   BrowserRouter as Router,
   Switch,
