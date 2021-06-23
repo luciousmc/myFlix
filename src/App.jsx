6d43bd30fd8488ee6f9bb5ba159deda4039a7163
Marlon Clay <luciousmc@icloud.com>
2021-06-23 14:04:36 -0700

    firebase auth configured app keeps track of login status app pushes auth user to redux store

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import HomeScreen from './pages/HomeScreen';
 import {
@@ -7,9 +7,28 @@ import {
   Route
 } from 'react-router-dom';
 import Login from './pages/Login';
+import { auth } from '../firebase';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout, selectUser } from './slices/userSlice';
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(userAuth => {
+      if (userAuth) {
+        dispatch(login({
+          uid: userAuth.uid,
+          email: userAuth.email
+        }))
+      } else {
+        dispatch(logout)
+      }
+    })
+
+    return unsubscribe;
+  }, [])
 
   return (
     <div className="app">
--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import appReducer from "../slices/appSlice";
+import userReducer from "../slices/userSlice";
 
 
 export const store = configureStore({
   reducer: {
-    app: appReducer
+    user: userReducer
   }
 })
 
