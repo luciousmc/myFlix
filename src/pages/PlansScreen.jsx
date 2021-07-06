4dfe0c4d01765138770530268fb1d80595735ed0
Marlon Clay <luciousmc@icloud.com>
2021-07-06 12:18:25 -0700

    adds stripe-js dependency for stripe checkout

--- src/pages/PlansScreen.jsx
+++ src/pages/PlansScreen.jsx
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import './PlansScreen.css';
 import { db } from './../../firebase';
+import { useSelector } from 'react-redux';
+import { selectUser } from '../slices/userSlice';
+import { loadStripe } from '@stripe/stripe-js';
+
 
 function PlansScreen() {
-  const [product, setProduct] = useState([]);
+  const [products, setProducts] = useState([]);
+  const user = useSelector(selectUser);
 
   useEffect(() => {
     db.collection('products')
@@ -22,14 +27,52 @@ function PlansScreen() {
             }
           })
         })
-        setProduct(products)
+        setProducts(products)
       })
   }, [])
 
+  const loadCheckout = async (priceId) => {
+    const docRef = await db.collection('customers')
+      .doc(user.uid)
+      .collection('checkout_sessions')
+      .add({
+        price: priceId,
+        success_url: window.location.origin,
+        cancel_url: window.location.origin
+      });
+
+    docRef.onSnapshot(async (snap) => {
+      const { error, sessionId } = snap.data();
+
+      if (error) {
+        alert('An error occurred: ', error.mesage);
+      }
+
+      if (sessionId) {
+        const stripe = await loadStripe('pk_test_51J9EggKGDeEymhK9BrGQ6ZGCEwUz0w5bCN7YdFwcfTVDF9sJ1GQEqsInjJmJ5nGqm3vKMoEilqAr7sr6EQmt8gof001hnCvMKb')
+
+        stripe.redirectToCheckout({ sessionId })
+      }
+    })
+  }
 
   return (
     <div className='plansScreen'>
-      
+      {Object.entries(products).map(([productId, productData]) => {
+        // TODO: Check if user subscription is active
+        return (
+          <div className="planScreen__plan">
+            <div className="planScreen__info">
+              <h5>{productData.name}</h5>
+              <h6>{productData.description}</h6>
+            </div>
+
+            <button onClick={() => loadCheckout(productData.prices.priceId)}>
+              Subscribe
+            </button>
+          </div>
+        );
+      })}
     </div>
   )
 }
