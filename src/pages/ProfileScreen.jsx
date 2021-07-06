6b60b2d984ac6062d3545abaaac894d265b752c4
Marlon Clay <luciousmc@icloud.com>
2021-07-06 11:20:11 -0700

    stripe setup update plans to retrieve price data

--- src/pages/ProfileScreen.jsx
+++ src/pages/ProfileScreen.jsx
@@ -4,6 +4,7 @@ import Nav from '../components/Nav';
 import { selectUser } from '../slices/userSlice';
 import './ProfileScreen.css';
 import { auth } from '../../firebase';
+import PlansScreen from './PlansScreen';
 
 function ProfileScreen() {
   const user = useSelector(selectUser);
@@ -25,6 +26,8 @@ function ProfileScreen() {
             <h2>{user.email}</h2>
             <div className="profileScreen__plans">
               <h3>Plans</h3>
+
+              <PlansScreen />
               
               <button
                 onClick={() => auth.signOut()}
